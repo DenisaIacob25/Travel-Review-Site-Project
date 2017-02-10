d08423ba3edea9a7d6ed5442c979d1dd42dd9df3
Denisa Iacob <denisaiacob18@gmail.com>
2017-02-09 19:09:32 -0500

    input fields into models

--- TravelReviewSite/Models/Category.cs
+++ TravelReviewSite/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,11 @@ namespace TravelReviewSite.Models
 {
     public class Category
     {
+        [Key]
+        public int CategoryID { get; set; }
+        public string Country { get; set; }
+        public DateTime DatePosted { get; set; }
+       
+        public virtual ICollection<Review> Reviews { get; set; }
     }
 }
--- TravelReviewSite/Models/Review.cs
+++ TravelReviewSite/Models/Review.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,17 @@ namespace TravelReviewSite.Models
 {
     public class Review
     {
+        [Key]
+        public int ReviewID { get; set; }
+        public string Username { get; set; }
+        public DateTime DatePosted { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Topic { get; set; }    
+        public string Description { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
